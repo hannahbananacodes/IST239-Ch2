697ad8567d30bedab76c0061ef3fa8415c8cbbe2
Hannah Holmes <hjholmes@hawaii.edu>
2023-08-24 14:57:06 +0200

    completed assignment: favorite movie page. added styles and images

---
+++ myFavMovie.js
@@ -0,0 +1,10 @@
+// Hannah Holmes
+
+document.write("<h2 style=\"color:hotpink; font-weight:700; font-style:italic\">Free Guy</h2>");
+document.write("<p style=\"color:red; font-weight: 900\">ALERT: spoilers!</p>")
+document.write("<p style= \"background-color: rgba(230, 40, 255, 0.7)\">Free Guy is a 2021 film about a man named \"Guy\" who realizes that he is a video game character and that he is living a very rigid, repetitive life within the game's story.");
+document.write(" Guy falls in love with the woman who helped create the basis of the video game, who he calls \"Molotov Girl\" (A.K.A., Millie).");
+document.write(" While leveling up his character's stats, which non-player characters aren't supposed to be able to do, Guy helps Millie prove that the game is intellectual property belonging to her and the game's co-creator, Keys.");
+document.write(" Millie and Keys are able to take back the game and restore it to its original form: A totally open-world game where anything is possible.");
+document.write(" Ultimately, Guy comes to accept that he and Millie are from different worlds and is happy living in the new game, where he can do anything he wants.");
+document.write(" Millie finds out that Keys molded Guy's character after his own feelings towards her, which is why Guy fell in love with her character in the game, and she realizes that she loves Keys, too.</p>");
